0bcd5df08c6501f72369dfaae6d4ba521369bca7
K5555kanafgo <jinjingshouta09@gmail.com>
2024-01-27 17:12:10 +0900

    ２か所　修正後

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  def  create
+    @user = User.new(params[:id])
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def show
     @user = User.find(params[:id])
     @prototypes = @user.prototypes
