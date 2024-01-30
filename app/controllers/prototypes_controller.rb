5a2ea02694797973f27ad4a6d3b10bf98e4e7486
K5555kanafgo <jinjingshouta09@gmail.com>
2024-01-30 21:02:49 +0900

    その他の機能修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,7 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_prototype, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -19,21 +21,18 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
-      redirect_to root_path
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype.id)
     else
-      render :edit, status: :unprocessable_entity
+      render :edit
     end
   end
 
@@ -48,8 +47,12 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def authenticate_user!
-    unless user_signed_in?
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user == @prototype.user
       redirect_to action: :index
     end
   end
